b033da6cdd8a9ca92bb117f61151b9124bac0afe
Joseph Aveltsev <aveltsev@gmail.com>
2019-09-24 21:02:28 +0300

    1/2 is done

--- UniversityApp/StudentCollection.cs
+++ UniversityApp/StudentCollection.cs
@@ -8,24 +8,66 @@ namespace UniversityApp
 {
     class StudentCollection
     {
+        // Delegates
+        public delegate void StudentListHandler(object source, StudentListHandlerEventArgs args);
+
+        // Events
+        public event StudentListHandler StudentCountChanged;
+        public event StudentListHandler StudentRefferenceChanged;
+
         // Fields
         private List<Student> listOfStudents = new List<Student>();
 
         // Constructors
+
         // Methods
         public void AddDefaults()
         {
-            ListOfStudents.Add(new Student("Judy", "Taylor", new DateTime(1962, 11, 6),
-                Education.Specialist, 5));
-            ListOfStudents.Add(new Student("Omar", "Dar", new DateTime(1992, 5, 21),
-                Education.Bachelor, 3));
-            ListOfStudents.Add(new Student("Evelyn", "Devis", new DateTime(1994, 6, 26),
-                Education.Specialist, 6));
-            ListOfStudents.Add(new Student("So", "Karlin", new DateTime(1975, 1, 11),
-                Education.Bachelor, 2));
+            Student[] students =
+            {
+                new Student("Judy", "Taylor", new DateTime(1962, 11, 6),
+                Education.Specialist, 5),
+                new Student("Omar", "Dar", new DateTime(1992, 5, 21),
+                Education.Bachelor, 3),
+                new Student("Evelyn", "Devis", new DateTime(1994, 6, 26),
+                Education.Specialist, 6),
+                new Student("So", "Karlin", new DateTime(1975, 1, 11),
+                Education.Bachelor, 2)
+            };
+
+            foreach(Student st in students)
+            {
+                StudentCountChanged?.Invoke(this,
+                    new StudentListHandlerEventArgs("ListOfStudents", "Default students were added.", st));
+            }
         }
 
-        public void AddStudents(params Student[] students) => ListOfStudents.AddRange(students);
+        public void AddStudents(params Student[] students)
+        {
+            ListOfStudents.AddRange(students);
+            foreach(Student st in students)
+            {
+                StudentCountChanged?.Invoke(this,
+                new StudentListHandlerEventArgs("ListOfStudents", "Some students were added.", st));
+            }
+            
+        }
+
+        public bool Remove(int j)
+        {
+            try
+            {
+                listOfStudents.RemoveAt(j);
+                StudentCountChanged?.Invoke(this,
+                    new StudentListHandlerEventArgs("ListOfStudents", "Student was removed.", ListOfStudents[j]));
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+            return true;
+        }
 
         public override string ToString()
         {
@@ -51,5 +93,18 @@ namespace UniversityApp
 
         // Properties
         public List<Student> ListOfStudents { get; set; }
+        public string CollectionName { get; set; }
+
+        // Indexator.
+        public Student this[int index]
+        {
+            get => ListOfStudents[index];
+            set
+            {
+                ListOfStudents[index] = value;
+
+                StudentRefferenceChanged?.Invoke(this, new StudentListHandlerEventArgs("ListOfStudents", "The element was changed.", value));
+            }
+        }
     }
 }
---
+++ UniversityApp/StudentListHandlerEventArgs.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversityApp
+{
+    class StudentListHandlerEventArgs : EventArgs
+    {
+        public string CollectionName { get; set; }  
+        public string CollectionChanges { get; set; }
+        public Student GetStudent { get; set; }
+
+        public StudentListHandlerEventArgs(string name, string changes,
+            Student student)
+        {
+            CollectionName = name;
+            CollectionChanges = changes;
+            GetStudent = student;
+        }
+
+        public StudentListHandlerEventArgs() { }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
